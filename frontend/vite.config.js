9caf6d0b74ec7124050c8c0eba96e919319e8f28
PratikMane@0112 <pratikmane610@gmail.com>
2024-11-15 18:08:47 +0530

    add base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,42 +1,22 @@
-// import { defineConfig } from 'vite'
-// import reactRefresh from '@vitejs/plugin-react-refresh'
-// import svgrPlugin from 'vite-plugin-svgr'
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import svgrPlugin from 'vite-plugin-svgr'
 
-// // see all documentation here https://vitejs.dev/config/
-// export default defineConfig({
-//   // This changes the out put dir from dist to build change as your need
-//   // comment this out if that isn't relevant for your project
-//   plugins: [
-//     reactRefresh(),
-//     svgrPlugin({
-//       svgrOptions: {
-//         icon: true,
-//         // ...svgr options (https://react-svgr.com/docs/options/)
-//       },
-//     }),
-//   ],
-//   server: {
-//     port: 3000
-//   }
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
-
+// see all documentation here https://vitejs.dev/config/
 export default defineConfig({
+  // This changes the out put dir from dist to build change as your need
+  // comment this out if that isn't relevant for your project
+  base: '/',
   plugins: [
-    react(),
+    reactRefresh(),
     svgrPlugin({
       svgrOptions: {
         icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
       },
     }),
   ],
-  build: {
-    outDir: 'dist', // Ensure this matches your output directory on Vercel
-  },
   server: {
-    port: 3000,
-  },
-  base: '/', // Ensure this is correct for your deployment
-});
+    port: 3000
+  }
+})

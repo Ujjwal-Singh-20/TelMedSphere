a06c0db10d4e3458a422c7950b41e8fad38195a5
PratikMane@0112 <pratikmane610@gmail.com>
2025-01-06 03:11:59 +0530

    import index.css

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/style.scss';
 import reportWebVitals from './reportWebVitals';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

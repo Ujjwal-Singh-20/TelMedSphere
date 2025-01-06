8e94d1e3d32f574a3ee88bb6ea822d295d57525c
Rucha-Ambaliya <rambaliyaforall@gmail.com>
2025-01-06 23:35:57 +0530

    Added new colors in theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,57 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("./colors");
+const { yearCalendarClasses } = require('@mui/x-date-pickers');
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/index.html"],
   theme: {
     extend: {
       colors: {
-        black: {
-          1: "#222",
-          2: "#1d1f2c",
-        },
-        white: "#f5f5f5",
-        blue: {
-          1: "#d4ddf1",
-          2: "#b0bbd8",
-          3: "#7584ae",
-          4: "#6575a5",
-          5: "#515e84",
-          6: "#3d4663",
-          7: "#282f42",
-          8: "#06075a",
-          9: "#4a4cb2",
-        },
-        yellow: {
-          1: "#f5f5dc",
-          2: "#f0e68c",
-          3: "#ffcc00",
-          4: "#ffd700",
-          5: "#eaa938",
-          6: "#af9500",
-        },
-        orange: {
-          1: "#FF8400",
-          2: "#af5b00",
-        },
-        grey: {
-          1: "#a9afc3",
-          2: "#b3b8d0",
-          3: "#6c757d",
-        },
-        social: {
-          fb: "#4a71c3",
-          fbHover: "#37528d",
-          google: "#a61c00",
-          googleHover: "#721300",
-          twitter: "#1da1f2",
-          twitterHover: "#1771a9",
-          instagram: "#DD2A7B",
-          instagramHover: "#a5215d",
-          whatsapp: "#25D366",
-          whatsappHover: "#1b9447",
-        },
+        black: colors.black,
+        blue: colors.blue,
+        yellow: colors.yellow,
+        orange: colors.orange,
+        grey: colors.grey,
+        white: colors.white,
+        teal: colors.teal,
+        green: colors.green,
+        purple: colors.purple,
+        social: colors.social
       },
       fontFamily: {
         sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
